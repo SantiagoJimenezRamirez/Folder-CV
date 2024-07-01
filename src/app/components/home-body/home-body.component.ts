22ac4f60853ceebb4eef1df55b772d853412d099
Santiago JR <snt1703@protonmail.com>
2024-07-01 14:52:01 -0500

    Fixes in the home page

--- src/app/components/home-body/home-body.component.ts
+++ src/app/components/home-body/home-body.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-body',
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
   templateUrl: './home-body.component.html',
   styleUrl: './home-body.component.scss'
 })
-export class HomeBodyComponent {
+export class HomeBodyComponent implements OnInit {
+
+  ngOnInit(): void {
+    
+  }
 
 }
