9bef08ba8f1f5dbcf8bea70f4667537f35692f41
Santiago JR <snt1703@protonmail.com>
2024-07-08 22:24:09 -0500

    About me in development

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss'
+})
+export class ModalComponent  {
+  showTarget = true
+}
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -4,13 +4,14 @@ import { HomeBodyComponent } from "../../components/home-body/home-body.componen
 import { FooterComponent } from "../../components/footer/footer.component";
 import { AboutMeComponent } from "../../components/about-me/about-me.component";
 import { LoaderComponent } from "../../shared/loader/loader.component";
+import { TargetComponent } from "../../shared/target/target.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [NavBarComponent, HomeBodyComponent, FooterComponent, AboutMeComponent, LoaderComponent]
+    imports: [NavBarComponent, HomeBodyComponent, FooterComponent, AboutMeComponent, LoaderComponent, TargetComponent]
 })
 export class HomeComponent {
 
